f191b65b5da105ce552ea80557bab1c51bcb214d
Mohamed-rirash <Mohamedrirash12045@gmail.com>
2023-08-28 16:49:44 +0300

    andvanced bmi calculator

--- day2/BMI calculator.py
+++ day2/BMI calculator.py
@@ -2,5 +2,21 @@ weight = float(input("Enter your weight: "))
 height = float(input("Enter your height: "))
 
 bmi = weight / height ** 2
+print(f"your body mass index is {bmi}")
 
-print(f"your body mass index is {int(bmi)}")
+
+if bmi < 18.5:
+    print("Your are underweight!\n Please eat more")
+elif bmi > 18.5:
+    if bmi < 25:
+        print("your weight is normal")
+elif bmi > 25:
+    if bmi < 30:
+        print("you are overweight")
+elif bmi > 30:
+    if bmi < 35:
+        print("you are obese")
+elif bmi > 35:
+    print("you are clinically obese")
+else:
+    print("please check your weight and height ")
