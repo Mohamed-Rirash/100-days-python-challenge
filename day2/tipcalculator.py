ef0e80f62a92507dd05e65a510a8d5648f5a53e4
Mohamed-rirash <Mohamedrirash12045@gmail.com>
2023-08-28 11:37:02 +0300

    tip calculator

---
+++ day2/tipcalculator.py
@@ -0,0 +1,14 @@
+# wellcom message
+#ask the total bill
+# ask tip percentage 10 , 12 ,15
+# ask how many people are split the bill
+# calculate tip calculate
+# print what each person will pay 
+print("welcomt to Tip calculator")
+totalBill = float(input("Enter a total bill: "))
+tipPercentage = int(input("Enter tip percentage 10 , 12 ,15: "))
+numberOfPeople = int(input("how many people are split the bill: "))
+
+billperperson = (totalBill * (tipPercentage / 100) + totalBill) / numberOfPeople
+
+print(f"each person will pay: ${round(billperperson, 2)}")
