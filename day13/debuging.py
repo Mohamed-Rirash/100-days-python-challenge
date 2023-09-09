f3719980c022697dec3396244a701d277954c3d0
Mohamed-rirash <Mohamedrirash12045@gmail.com>
2023-09-09 16:14:14 +0300

    debuging

---
+++ day13/debuging.py
@@ -0,0 +1,11 @@
+# from random import randint
+# dice = ["1","2","3","4","5","6"]
+# dice_num = randint(0,5)
+# print(dice[dice_num])
+
+num = int(input("entr a number"))
+
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
