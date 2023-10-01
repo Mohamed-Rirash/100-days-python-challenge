3a271f63c36ff0136f3547f129bff8946937465a
Mohamed Rirash <Mohamedrirash12045@gmail.com>
2023-10-01 19:17:34 +0300

    nato phonetic alphabet game

---
+++ day26/main.py
@@ -0,0 +1,60 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# #TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+# data = pandas.read_csv("day26/nato_phonetic_alphabet.csv")
+
+
+# # datadic = {letter:code.to_dict() for letter,code in data.iterrows()}
+# data_dict = dict(zip(data['letter'], data['code']))
+
+
+# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+# user_name = input("what is your name: ").upper()
+
+# user_name_lsit = [letter for letter in user_name.split(" ")]
+
+# result = [data_dict.get(letter, letter) for letter in user_name if letter.isalpha()]
+
+# print(result)
+
+
+import pandas
+
+data = pandas.read_csv("day26/nato_phonetic_alphabet.csv")
+
+
+
+
+
+# #TODO 1. Create a dictionary in this format:
+phenatic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+
+
+
+# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_name = input("what is your name: ").upper()
+resutl = [phenatic_dict[letter] for letter in user_name ]
+
+print(resutl)
